5a7f2ce64d8c8447286ed84628a27e9e344a5e65
maxyee <eyaminkhan00@gmail.com>
2023-03-12 01:02:33 +0600

    getting no concept about new structure of program.cs file

---
+++ dependency_console_app/dependency_console/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// DI, Serilog, Settings
+
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.IO;
+//Console.WriteLine("Hello, World!");
+
+var builder = new ConfigurationBinder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+
+Log.Logger.Information("Application Starting");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+
+    })
+    .UseSerilog()
+    .Build();
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables();
+}
